05be9657b3d1dbf8e47e827270bdfc3b1b6411bd
acarrill <a.jimenezcarr@alumnos.urjc.es>
2026-01-14 13:55:27 +0100

    feat: initialize project

---
+++ similarity-aggregator/src/main/java/com/example/similarityaggregator/SimilarityAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.similarityaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarityAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimilarityAggregatorApplication.class, args);
+	}
+
+}
---
+++ similarity-aggregator/src/test/java/com/example/similarityaggregator/SimilarityAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.similarityaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarityAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
